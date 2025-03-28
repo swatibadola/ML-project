4d51a88436490dc1e27f779002f93fe09deb4635
swatibadola <sb3875@dseu.ac.in>
2025-03-28 15:31:03 +0530

    Data transformation files added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass   #used to create class variables
 
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -53,4 +55,8 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data =  obj.initiate_data_ingestion()
+
+
+data_transformation = DataTransformation()
+data_transformation.initiate_data_tranformation(train_data, test_data)
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,33 @@
 # ex: if I want to create a mongodb for the database we can use this file. 
 # If I want to save my model in the cloud I'll write the code for that here.
+
+import numpy as np
+import pandas as pd
+# pickle is a Python module that saves objects to a file and loads them later.
+
+# However, pickle cannot save some complex objects like:
+
+# Lambda functions (lambda x: x + 1)
+
+# Nested functions (functions inside functions)
+
+# Custom-defined objects with certain decorators
+
+# dill is an improved version of pickle that supports these complex objects.
+import dill
+
+import os
+import sys
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        # 'wb' -> write binary mode
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)    #used to serialize and save object
+
+    except Exception as e:
+        raise CustomException(e, sys)
